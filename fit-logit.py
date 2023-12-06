c86027e937e2a591bfa2fa901949250b7e4dd671
Pepijn Bruinaars <pepijnbruinaars@xs4all.nl>
2023-12-06 09:58:19 +0100

    Ran logit on many different parameters

--- fit-logit.py
+++ fit-logit.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 import multiprocessing as mp
 
+from sklearn.model_selection import cross_val_score
+
 
 def fit_logit(param):
-    test_digits = pd.read_csv("data/test_digits.csv").values
-    test_labels = pd.read_csv("data/test_labels.csv").values
     train_digits = pd.read_csv("data/train_digits.csv").values
     train_labels = pd.read_csv("data/train_labels.csv").values
     logit = LogisticRegression(
@@ -15,30 +15,52 @@ def fit_logit(param):
         penalty="l1",
         solver="saga",
         multi_class="multinomial",
-        max_iter=300,
+        max_iter=500,
         verbose=2,
     )
-    logit.fit(train_digits, train_labels.ravel())
-    score = logit.score(test_digits, test_labels)
-    print("done fitting param: ", param, " score: ", score)
-    return score
+    # Get cross-validated accuracy, recall and precision
+    scores = cross_val_score(logit, train_digits, train_labels.ravel(), cv=5)
+
+    mean_score = np.mean(scores)
+    std_score = np.std(scores)
+
+    return mean_score, std_score
 
 
 def main():
     # Fit multinomial logistic regression with LASSO penalty
     # Use cross-validation to select regularization parameter
     # Use all cores available
-    param_space = np.logspace(-4, 0, 30)
+    param_space = np.logspace(-4, 0, 50)
 
     with mp.Pool(processes=mp.cpu_count()) as pool:
         scores = pool.map(fit_logit, param_space)
 
+        (
+            mean_accuracy,
+            std_accuracy,
+        ) = zip(*scores)
+        dataframe = pd.DataFrame(
+            {
+                "param": param_space,
+                "mean_accuracy": mean_accuracy,
+                "std_accuracy": std_accuracy,
+            }
+        )
         # save scores
-        np.savetxt("multinomial-scores.csv", scores, delimiter=",")
+        dataframe.to_csv("logit-lasso-scores.csv", index=False)
 
     # Plot scores
     plt.figure(figsize=(8, 6), dpi=300)
-    plt.plot(param_space, scores)
+    plt.plot(param_space, mean_accuracy)
+
+    # Plot error bars showing standard deviation with fill
+    plt.fill_between(
+        param_space,
+        np.array(mean_accuracy) - np.array(std_accuracy),
+        np.array(mean_accuracy) + np.array(std_accuracy),
+        alpha=0.2,
+    )
     plt.xscale("log")
     plt.xlabel("Regularization parameter")
     plt.ylabel("Accuracy")
